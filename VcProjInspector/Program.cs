aa11e17d95f1e71c2fe7ee161162322b8ef7d9a0
Mikhail Virovets <virovets64@gmail.com>
2018-04-14 19:35:47 +0300

    (+) Create project, utilize CommandLineParser

---
+++ VcProjInspector/Program.cs
@@ -0,0 +1,35 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+
+namespace VcProjInspector
+{
+  class Program
+  {
+    class Options
+    {
+      [Option('d', "dirs", Required = true, HelpText = "directories to scan.")]
+      public IEnumerable<string> IncludeDirectories { get; set; }
+
+      [Option('x', "exclude_dirs", Required = false, HelpText = "directories to exclude.")]
+      public IEnumerable<string> ExcludeDirectories { get; set; }
+    }
+
+    static void Main(string[] args)
+    {
+      CommandLine.Parser.Default.ParseArguments<Options>(args)
+        .WithParsed<Options>(opts => RunOptionsAndReturnExitCode(opts))
+        .WithNotParsed<Options>((errs) => HandleParseError(errs));
+    }
+
+    static void RunOptionsAndReturnExitCode(Options options)
+    {
+      Environment.ExitCode = 0;
+    }
+
+    static void HandleParseError(IEnumerable<Error> errors)
+    {
+      Environment.ExitCode = 1;
+    }
+  }
+}
