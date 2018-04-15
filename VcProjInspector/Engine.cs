90037cadd67891bf989b131604696ccf575ba47e
Mikhail Virovets <virovets64@gmail.com>
2018-04-15 13:01:48 +0300

    (*) Add collections of solutions and projects

--- VcProjInspector/Engine.cs
+++ VcProjInspector/Engine.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Reflection;
 using System.Text;
+using Microsoft.Build.Construction;
 
 namespace VcProjInspector
 {
@@ -9,6 +11,8 @@ namespace VcProjInspector
   {
     private List<Inspection> inspections = new List<Inspection>();
     private List<Defect> defects = new List<Defect>();
+    private Dictionary<String, SolutionFile> solutions = new Dictionary<String, SolutionFile>();
+    private Dictionary<String, ProjectRootElement> projects = new Dictionary<String, ProjectRootElement>();
 
     private void collectInspections()
     {
@@ -19,6 +23,53 @@ namespace VcProjInspector
         if (attribute != null)
           inspections.Add((Inspection)Activator.CreateInstance(type));
       }
+    }
+
+    private void collectFiles(Program.Options options)
+    {
+      foreach (var dir in options.IncludeDirectories)
+      {
+        foreach (var filename in Directory.GetFiles(dir, "*", SearchOption.AllDirectories))
+        {
+          switch (Path.GetExtension(filename))
+          {
+            case ".sln":
+              addSolution(filename);
+              break;
+            case ".vcxproj":
+              addProject(filename);
+              break;
+          }
+        }
+      }
+    }
+
+    private void addSolution(String filename)
+    {
+      SolutionFile solution = null;
+      try
+      {
+        solution = SolutionFile.Parse(filename);
+      }
+      catch (Exception e)
+      {
+        AddDefect(new Defect { Path = filename, Description = "Can't open solution: " + e.Message });
+      }
+      solutions.Add(filename, solution);
+    }
+
+    private void addProject(string filename)
+    {
+      ProjectRootElement project = null;
+      try
+      {
+        project = ProjectRootElement.Open(filename);
+      }
+      catch (Exception e)
+      {
+        AddDefect(new Defect { Path = filename, Description = "Can't open project: " + e.Message });
+      }
+      projects.Add(filename, project);
     }
 
     private void runInspections()
@@ -32,6 +83,7 @@ namespace VcProjInspector
     public void Run(Program.Options options)
     {
       collectInspections();
+      collectFiles(options);
       runInspections();
     }
 
@@ -48,5 +100,20 @@ namespace VcProjInspector
       defects.Add(defect);
     }
 
+    public IReadOnlyDictionary<String, SolutionFile> Solutions
+    {
+      get
+      {
+        return solutions;
+      }
+    }
+
+    public IReadOnlyDictionary<String, ProjectRootElement> Projects
+    {
+      get
+      {
+        return projects;
+      }
+    }
   }
 }
--- VcProjInspector/IEngine.cs
+++ VcProjInspector/IEngine.cs
@@ -7,5 +7,15 @@ namespace VcProjInspector
   public interface IEngine
   {
     void AddDefect(Defect defect);
+
+    IReadOnlyDictionary<String, Microsoft.Build.Construction.SolutionFile> Solutions
+    {
+      get;
+    }
+
+    IReadOnlyDictionary<String, Microsoft.Build.Construction.ProjectRootElement> Projects
+    {
+      get;
+    }
   }
 }
