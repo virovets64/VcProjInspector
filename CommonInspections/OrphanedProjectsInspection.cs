ccb4ae83517967a4fb34187f187dcb6b43424ed1
Mikhail Virovets <virovets64@gmail.com>
2018-04-29 19:58:44 +0300

    (+) Orphaned projects inspection

---
+++ CommonInspections/OrphanedProjectsInspection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using InspectorCore;
+
+namespace CommonInspections
+{
+  [InspectionClass]
+  class OrphanedProjectsInspection : Inspection
+  {
+    [DefectClass(Code = "B3", Severity = DefectSeverity.Warning)]
+    private class Defect_ProjectIsOrphan : Defect
+    {
+      public Defect_ProjectIsOrphan(String filename) :
+        base(filename, 0, SDefect.ProjectIsOrphan)
+      { }
+    }
+
+    protected override void run()
+    {
+      foreach (var project in Model.Entities<VcProjectEntity>())
+        if(!project.LinksTo<VcProjectReference>().Any())
+          Context.AddDefect(new Defect_ProjectIsOrphan(project.PathFromBase));
+    }
+  }
+}
